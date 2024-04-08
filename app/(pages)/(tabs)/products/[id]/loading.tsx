935a3c7af9e58ec40958bdf5db14457a36e5a993
kmonguu <skdus8009@gmail.com>
2024-04-08 23:33:21 +0900

    feat: product detail skeleton loading

---
+++ app/(pages)/(tabs)/products/[id]/loading.tsx
@@ -0,0 +1,19 @@
+import { PhotoIcon } from "@heroicons/react/24/solid";
+
+export default function Loading() {
+  return (
+    <div className="animate-pulse flex flex-col p-5 gap-5 ">
+      <div className="flex justify-center items-center rounded-md border-neutral-700 border-4 aspect-square">
+        <PhotoIcon className="h-28 text-neutral-700" />
+      </div>
+      <div className="flex gap-2 items-center">
+        <div className="size-14 rounded-full bg-neutral-700" />
+        <div className="flex flex-col gap-1">
+          <div className="h-5 w-40 bg-neutral-700 rounded-md" />
+          <div className="h-5 w-20 bg-neutral-700 rounded-md" />
+        </div>
+      </div>
+      <div className="h-5 w-80 bg-neutral-700 rounded-md" />
+    </div>
+  );
+}
