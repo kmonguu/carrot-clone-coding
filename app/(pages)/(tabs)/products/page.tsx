0cc60c458864faadea25a1af2eeacccaa3f9eb70
kmonguu <skdus8009@gmail.com>
2024-04-10 18:16:50 +0900

    feat: 상품 페이지에서 상품 추가 페이지로 이동할 수 있는 버튼 생성

--- app/(pages)/(tabs)/products/page.tsx
+++ app/(pages)/(tabs)/products/page.tsx
@@ -2,6 +2,8 @@ import ProductList from "@/app/common/components/products/list";
 import db from "@/app/libs/db";
 import { Prisma } from "@prisma/client";
 import { getInitialProducts } from "./action";
+import Link from "next/link";
+import { PlusIcon } from "@heroicons/react/24/solid";
 
 export type InitialProducts = Prisma.PromiseReturnType<
   typeof getInitialProducts
@@ -12,6 +14,12 @@ export default async function Products() {
   return (
     <div>
       <ProductList initialProducts={initialProducts} />
+      <Link
+        href="/products/add"
+        className="flex justify-center items-center bg-orange-500 rounded-full size-16 fixed bottom-24 right-8 text-white transition-colors hover:bg-orange-400"
+      >
+        <PlusIcon className="size-10" />
+      </Link>
     </div>
   );
 }
